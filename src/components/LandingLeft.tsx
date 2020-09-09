30bb8c278ce8770f7dc1552d4854023dc9855b85
Renato Guimaraes Nunes <renatognunes@hotmail.com>
2020-09-08 23:10:14 -0300

    eslint fixes

--- src/components/LandingLeft.tsx
+++ src/components/LandingLeft.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { ReactComponent as LandingImage } from '../assets/undraw_heatmap_uyye.svg';
-import { ReactComponent as LandingImage1 } from '../assets/undraw_feeling_proud_qne1.svg';
+import { ReactComponent as LandingImage } from '../assets/undraw_feeling_proud_qne1.svg';
 
 function LandingLeft(): JSX.Element {
     return (
         <div className="landing-image">
-            <LandingImage1 />
+            <LandingImage />
         </div>
     );
 }
